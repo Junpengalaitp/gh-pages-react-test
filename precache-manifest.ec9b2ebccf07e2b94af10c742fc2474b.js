5048a370727ae3eab7d67fd8332e832a339c4baa
hejp009 <hejunpeng2012@hotmail.com>
2020-03-02 09:44:24 +0800

    Updates

---
+++ precache-manifest.ec9b2ebccf07e2b94af10c742fc2474b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60dbdfa8d6ec512d0259c4098a431337",
+    "url": "/gh-pages-react-test/index.html"
+  },
+  {
+    "revision": "d69996facccf9de76332",
+    "url": "/gh-pages-react-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b1c891c76974b2da237d",
+    "url": "/gh-pages-react-test/static/js/2.ff4eaca5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/gh-pages-react-test/static/js/2.ff4eaca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69996facccf9de76332",
+    "url": "/gh-pages-react-test/static/js/main.9c0bf595.chunk.js"
+  },
+  {
+    "revision": "7c51425b2aff4d4dcdf4",
+    "url": "/gh-pages-react-test/static/js/runtime-main.1fd93572.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
